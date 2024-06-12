6575fb1cb223471d42e73c0e42593e6ef3712111
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-06-12 18:20:12 -0300

    service responsible for searching for a user using their username

---
+++ src/main/java/api/todo_list_api/services/UserService.java
@@ -0,0 +1,27 @@
+package api.todo_list_api.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import api.todo_list_api.repository.UserRepository;
+
+@Service
+public class UserService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        var user = repository.findByUsername(username);
+        if (user != null) {
+            return user;
+        } else {
+            throw new UsernameNotFoundException("Username: " + username + " not found!");
+        }
+    }
+
+}
