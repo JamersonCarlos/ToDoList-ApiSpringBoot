754d4219be21fcfe69dc4c28e033924fe47dd222
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-06-12 22:55:50 -0300

    Create UserVO.java

---
+++ src/main/java/api/todo_list_api/VO/UserVO.java
@@ -0,0 +1,62 @@
+package api.todo_list_api.VO;
+
+import java.io.Serializable;
+
+public class UserVO implements Serializable {
+
+    private String username;
+    private String fullName;
+
+    public UserVO() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserVO other = (UserVO) obj;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (fullName == null) {
+            if (other.fullName != null)
+                return false;
+        } else if (!fullName.equals(other.fullName))
+            return false;
+        return true;
+    }
+
+    
+
+} 
