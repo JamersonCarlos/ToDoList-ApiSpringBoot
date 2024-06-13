3fef001be7445c3ce425468ecab19715cfd26a1a
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-06-12 22:54:04 -0300

    Update UserModel.java

--- src/main/java/api/todo_list_api/models/UserModel.java
+++ src/main/java/api/todo_list_api/models/UserModel.java
@@ -5,6 +5,7 @@ import java.util.Collection;
 
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.Mapping;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
@@ -14,57 +15,167 @@ import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.Email;
 
-
 @Entity
 @Table(name = "users")
 public class UserModel implements UserDetails, Serializable {
 
-
     @Id
-    @GeneratedValue(strategy =  GenerationType.IDENTITY)
-    private Long id; 
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
 
     @Column(nullable = false, unique = true)
     @Email
-    private String email; 
+    private String username;
 
-    @Column(name = "full_name")
-    private String fullName; 
+    @Column(name = "full_name", nullable = false)
+    private String fullName;
 
-    @Column
-    private String password; 
-    
+    @Column(nullable = false)
+    private String password;
+
     @Column(name = "account_non_expired")
-    private String accountNonExpired; 
+    private Boolean accountNonExpired = true;
 
     @Column(name = "account_non_locked")
-    private String accountNonLocked; 
+    private Boolean accountNonLocked = true;
 
     @Column(name = "credentials_non_expired")
-    private String credentialsNonExpired;
+    private Boolean credentialsNonExpired = true;
 
     @Column
-    private Boolean enabled; 
+    private Boolean enabled = true;
 
     public UserModel() {
     }
 
-
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null; 
+        return null;
     }
 
     @Override
     public String getPassword() {
-        return this.password; 
+        return this.password;
     }
 
     @Override
     public String getUsername() {
-        return this.email; 
+        return this.username;
     }
 
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-    
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Boolean getAccountNonExpired() {
+        return accountNonExpired;
+    }
+
+    public void setAccountNonExpired(Boolean accountNonExpired) {
+        this.accountNonExpired = accountNonExpired;
+    }
+
+    public Boolean getAccountNonLocked() {
+        return accountNonLocked;
+    }
+
+    public void setAccountNonLocked(Boolean accountNonLocked) {
+        this.accountNonLocked = accountNonLocked;
+    }
+
+    public Boolean getCredentialsNonExpired() {
+        return credentialsNonExpired;
+    }
+
+    public void setCredentialsNonExpired(Boolean credentialsNonExpired) {
+        this.credentialsNonExpired = credentialsNonExpired;
+    }
+
+    public Boolean getEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((username == null) ? 0 : username.hashCode());
+        result = prime * result + ((fullName == null) ? 0 : fullName.hashCode());
+        result = prime * result + ((password == null) ? 0 : password.hashCode());
+        result = prime * result + ((accountNonExpired == null) ? 0 : accountNonExpired.hashCode());
+        result = prime * result + ((accountNonLocked == null) ? 0 : accountNonLocked.hashCode());
+        result = prime * result + ((credentialsNonExpired == null) ? 0 : credentialsNonExpired.hashCode());
+        result = prime * result + ((enabled == null) ? 0 : enabled.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        UserModel other = (UserModel) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (username == null) {
+            if (other.username != null)
+                return false;
+        } else if (!username.equals(other.username))
+            return false;
+        if (fullName == null) {
+            if (other.fullName != null)
+                return false;
+        } else if (!fullName.equals(other.fullName))
+            return false;
+        if (password == null) {
+            if (other.password != null)
+                return false;
+        } else if (!password.equals(other.password))
+            return false;
+        if (accountNonExpired == null) {
+            if (other.accountNonExpired != null)
+                return false;
+        } else if (!accountNonExpired.equals(other.accountNonExpired))
+            return false;
+        if (accountNonLocked == null) {
+            if (other.accountNonLocked != null)
+                return false;
+        } else if (!accountNonLocked.equals(other.accountNonLocked))
+            return false;
+        if (credentialsNonExpired == null) {
+            if (other.credentialsNonExpired != null)
+                return false;
+        } else if (!credentialsNonExpired.equals(other.credentialsNonExpired))
+            return false;
+        if (enabled == null) {
+            if (other.enabled != null)
+                return false;
+        } else if (!enabled.equals(other.enabled))
+            return false;
+        return true;
+    }
+
 }
