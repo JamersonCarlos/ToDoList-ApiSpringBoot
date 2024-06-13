a8accefdb5450c00e24e006bab99199276f238d2
Jamerson Carlos <64469941+JamersonCarlos@users.noreply.github.com>
2024-06-12 22:53:46 -0300

    Update Mapper.java

--- src/main/java/api/todo_list_api/mapper/Mapper.java
+++ src/main/java/api/todo_list_api/mapper/Mapper.java
@@ -4,10 +4,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.modelmapper.ModelMapper;
+import org.modelmapper.PropertyMap;
+
+import api.todo_list_api.VO.UserVO;
+import api.todo_list_api.models.UserModel;
 
 public class Mapper {
 
     private static ModelMapper mapper = new ModelMapper();
+
+
 
     public static <O, D> D parseObject(O origin, Class<D> destination) {
         return mapper.map(origin, destination);
